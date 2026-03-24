26dd06ea434fd671a88b7b5f8884014d2a861cbb
Keaexe <pierreolivierpetit@proton.me>
2026-03-24 09:59:02 +0100

    fix(DataAccess): now throw exceptions

--- src/Interfaces/IDataAccess.java
+++ src/Interfaces/IDataAccess.java
@@ -6,6 +6,6 @@ import java.util.Dictionary;
 
 public interface IDataAccess {
     Dictionary<Integer,Item> getAllItems();
-    Item getItemById(Integer id);
-    Integer getItemCount();
+    Item getItemById(Integer id) throws Exception;
+    Integer getItemCount() throws Exception;
 }
