54388e508db4ab65046c7248af7a6b9b320fb013
Sbyspaceopera <70596078+Sbyspaceopera@users.noreply.github.com>
2026-03-27 13:24:40 +0100

    Snake case convention and small corrections

--- mysql/creation_script.sql
+++ mysql/creation_script.sql
@@ -3,116 +3,116 @@ USE POO_ADVANCED;
 
 CREATE TABLE Category (
     name VARCHAR(100) PRIMARY KEY,
-    description TEXT
+    description TEXT NOT NULL
 );
 
 CREATE TABLE Country (
     name VARCHAR(50) PRIMARY KEY,
-    isSupported BOOLEAN NOT NULL
+    is_supported BOOLEAN NOT NULL
 );
 
 CREATE TABLE Employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(50) NOT NULL,
-    lastName VARCHAR(100) NOT NULL,
-    hiringDate DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    hiring_date DATE NOT NULL,
     salary DECIMAL(8, 2) NOT NULL
 );
 
 CREATE TABLE Coupon (
     code VARCHAR(20) PRIMARY KEY,
-    discountPercentage DECIMAL(5, 2) NOT NULL
+    discount_percentage DECIMAL(5, 2) NOT NULL
 );
 
 CREATE TABLE Item (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    priceExVAT DECIMAL(8, 2) NOT NULL,
-    VATPercentage DECIMAL(5, 2) NOT NULL,
-    leftInStock INT NOT NULL,
+    price_ex_vat DECIMAL(8, 2) NOT NULL,
+    vat_percentage DECIMAL(5, 2) NOT NULL,
+    left_in_stock INT NOT NULL,
     description TEXT NOT NULL,
-    imagePath VARCHAR(255) NOT NULL,
-    categoryName VARCHAR(100) NOT NULL,
-    FOREIGN KEY (categoryName) REFERENCES Category(name)
+    image_url VARCHAR(255) NOT NULL,
+    category_name VARCHAR(100) NOT NULL,
+    FOREIGN KEY (category_name) REFERENCES Category(name)
 );
 
 CREATE TABLE Locality (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    zipCode VARCHAR(20),
-    isSupported BOOLEAN,
-    countryName VARCHAR(50),
-    FOREIGN KEY (countryName) REFERENCES Country(name)
+    name VARCHAR(255) NOT NULL,
+    zipCode VARCHAR(20) NOT NULL,
+    is_supported BOOLEAN NOT NULL,
+    country_name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (country_name) REFERENCES Country(name)
 );
 
 CREATE TABLE DeliveryAddress (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    isPickupPoint BOOLEAN,
-    numInStreet VARCHAR(20),
-    streetName VARCHAR(255),
-    box VARCHAR(20),
-    localityId INT,
-    FOREIGN KEY (localityId) REFERENCES Locality(id)
+    is_pickup_point BOOLEAN NOT NULL,
+    num_in_street VARCHAR(20) NOT NULL,
+    street_name VARCHAR(255) NOT NULL,
+    box VARCHAR(10) NULL,
+    locality_id INT NOT NULL,
+    FOREIGN KEY (locality_id) REFERENCES Locality(id)
 );
 
 CREATE TABLE Client (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(50),
-    lastName VARCHAR(100),
-    deliveryAddressId INT,
-    FOREIGN KEY (deliveryAddressId) REFERENCES DeliveryAddress(id)
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    delivery_address_id INT NOT NULL,
+    FOREIGN KEY (delivery_address_id) REFERENCES DeliveryAddress(id)
 );
 
-CREATE TABLE SchoolOfThough (
+CREATE TABLE SchoolOfThought (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    description TEXT
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
 );
 
 CREATE TABLE Referent (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    designation VARCHAR(25) NOT NULL,
-    firstName VARCHAR(50) NOT NULL,
-    lastName VARCHAR(100) NOT NULL,
-    birthDate DATE NOT NULL,
-    isAlive BOOLEAN NOT NULL,
+    designation VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    is_alive BOOLEAN NOT NULL,
     website VARCHAR(255) NULL,
     nickname VARCHAR(100) NULL,
-    schoolOfThoughId INT NULL,
-    FOREIGN KEY (schoolOfThoughId) REFERENCES SchoolOfThough(id)
+    school_of_thought_id INT NULL,
+    FOREIGN KEY (school_of_thought_id) REFERENCES SchoolOfThought(id)
 );
 
 CREATE TABLE OrderTable (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    creationTime DATETIME NOT NULL,
-    employeeId INT NOT NULL,
-    clientId INT NOT NULL,
-    FOREIGN KEY (employeeId) REFERENCES Employee(id),
-    FOREIGN KEY (clientId) REFERENCES Client(id)
+    creation_time DATETIME NOT NULL,
+    employee_id INT NOT NULL,
+    client_id INT NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES Employee(id),
+    FOREIGN KEY (client_id) REFERENCES Client(id)
 );
 
 CREATE TABLE OrderLine (
-    orderId INT NOT NULL,
-    itemId INT NOT NULL,
+    order_id INT NOT NULL,
+    item_id INT NOT NULL,
     quantity INT NOT NULL,
-    priceAtTheTime DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (orderId, itemId),
-    FOREIGN KEY (orderId) REFERENCES OrderTable(id),
+    price_at_the_time DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (order_id, itemId),
+    FOREIGN KEY (order_id) REFERENCES OrderTable(id),
     FOREIGN KEY (itemId) REFERENCES Item(id)
 );
 
 CREATE TABLE OrderCoupon (
-    orderId INT NOT NULL,
-    couponCode VARCHAR(20) NOT NULL,
-    PRIMARY KEY (orderId, couponCode),
-    FOREIGN KEY (orderId) REFERENCES OrderTable(id),
-    FOREIGN KEY (couponCode) REFERENCES Coupon(code)
+    order_id INT NOT NULL,
+    coupon_code VARCHAR(20) NOT NULL,
+    PRIMARY KEY (order_id, coupon_code),
+    FOREIGN KEY (order_id) REFERENCES OrderTable(id),
+    FOREIGN KEY (coupon_code) REFERENCES Coupon(code)
 );
 
 CREATE TABLE ItemReferent (
-    itemId INT NOT NULL,
-    referentId INT NOT NULL,
-    PRIMARY KEY (itemId, referentId),
+    item_id INT NOT NULL,
+    referent_id INT NOT NULL,
+    PRIMARY KEY (itemId, referent_id),
     FOREIGN KEY (itemId) REFERENCES Item(id),
-    FOREIGN KEY (referentId) REFERENCES Referent(id)
+    FOREIGN KEY (referent_id) REFERENCES Referent(id)
 );
