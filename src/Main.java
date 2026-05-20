77938d6eb7103f4273b09b19f39bff455b156678
Keaexe <pierreolivierpetit@proton.me>
2026-05-20 20:18:28 +0200

    fix: remove test in main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 import DataAccess.DBAccess;
-import Model.Referent;
-import UserInterface.MainWindow;
 import Interfaces.*;
+import Model.*;
+import UserInterface.MainWindow;
 
 void main() {
     var mainWindow = new MainWindow();
-    try{
-        IDataAccess dbAccess = new DBAccess();
-        System.out.println(dbAccess.toString());
-        dbAccess.addReferent(new Referent(null,
-                "test", "test", "test", LocalDate.now(), true,
-                1, null, null));
-    }catch (Exception e){
-        System.out.println("SWALALA");
-        System.out.println(e.getMessage());
-    }
 }
