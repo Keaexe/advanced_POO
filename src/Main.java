69265f6472fa431703050bf6d7f902f2cf773039
Sbyspaceopera <70596078+Sbyspaceopera@users.noreply.github.com>
2026-05-25 22:57:13 +0200

    Back to the old Main.java declaration to ensure compatibilty if professor use Langage Level < 25

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,22 @@ import Exceptions.DataAccessException;
 import UserInterface.MainWindow;
 import javax.swing.*;
 
-void main() {
-    var controller = new Controller();
-    var mainWindow = new MainWindow(controller);
-    controller.setUI(mainWindow);
-    var business = new BusinessLogic(controller);
-    try {
-        business.setDataAccess(new DBAccess());
-    } catch (DataAccessException e) {
-        JOptionPane.showMessageDialog(
-            mainWindow,
-            "Cannot access data",
-            "Error",
-            JOptionPane.ERROR_MESSAGE
-        );
+public class Main {
+    public static void main(String[] args) {
+        var controller = new Controller();
+        var mainWindow = new MainWindow(controller);
+        controller.setUI(mainWindow);
+        var business = new BusinessLogic(controller);
+        try {
+            business.setDataAccess(new DBAccess());
+        } catch (DataAccessException e) {
+            JOptionPane.showMessageDialog(
+                    mainWindow,
+                    "Cannot access data",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+        controller.setBusinessLogic(business);
     }
-    controller.setBusinessLogic(business);
 }
