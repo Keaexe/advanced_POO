947d61770e8845abff79d33c86f27245aa2f1edb
Sbyspaceopera <70596078+Sbyspaceopera@users.noreply.github.com>
2026-05-25 15:45:29 +0200

    Referent done

--- src/Model/Referent.java
+++ src/Model/Referent.java
@@ -1,6 +1,6 @@
 package Model;
 
-import Utils.StringUtils;
+import Utils.ValidationUtils;
 
 import java.time.LocalDate;
 
@@ -8,26 +8,26 @@ public class Referent {
     private Integer id;
     private String designation, firstName, lastName;
     private LocalDate birthDate;
-    private Boolean isAlive;
+    private boolean isAlive;
     private Integer idSchoolOfThought;
     private String website, nickname;
 
     public Referent(Integer id, String designation, String firstName, String lastName,LocalDate birthDate,
                     Boolean isAlive, Integer idSchoolOfThought, String website, String nickname){
         this.id = id;
-        this.designation = designation;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthDate = birthDate;
+        setDesignation(designation);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setBirthDate(birthDate);
         this.isAlive = isAlive;
         this.idSchoolOfThought = idSchoolOfThought;
-        this.website = website;
-        this.nickname = nickname;
+        setWebsite(website);
+        setNickname(nickname);
     }
 
-    public Referent(Integer id, String designation, String firstName, String lastName,LocalDate birthDate,
-                    Boolean isAlive, Integer idSchoolOfThought){
-        this(id,designation,firstName,lastName,birthDate,isAlive,idSchoolOfThought, null, null);
+    public Referent( String designation, String firstName, String lastName,LocalDate birthDate,
+                    Boolean isAlive, Integer idSchoolOfThought, String website, String nickname){
+        this(null,designation,firstName,lastName,birthDate,isAlive,idSchoolOfThought, website, nickname);
     }
 
     public Integer getId() {
@@ -38,14 +38,14 @@ public class Referent {
         return designation;
     }
     public void setDesignation(String designation){
-        this.designation = StringUtils.requireNotBlank(designation, "designation");
+        this.designation = ValidationUtils.validateString(designation, "designation", true, 100);
     }
 
     public String getFirstName() {
         return firstName;
     }
     public void setFirstName(String firstName){
-        this.firstName= StringUtils.requireNotBlank(firstName, "firstName");
+        this.firstName= ValidationUtils.validateString(firstName, "firstName", true, 50);
     }
 
 
@@ -53,14 +53,17 @@ public class Referent {
         return lastName;
     }
     public void setLastName(String lastName){
-        this.lastName = StringUtils.requireNotBlank(lastName, "lastName");
+        this.lastName = ValidationUtils.validateString(lastName, "lastName", true, 100);
     }
 
     public LocalDate getBirthDate() {
         return birthDate;
     }
+    public void setBirthDate(LocalDate birthDate){
+        this.birthDate = ValidationUtils.validateDate(birthDate,"Birth date", true,true,true,false);
+    }
 
-    public Boolean getIsAlive() {
+    public boolean getIsAlive() {
         return isAlive;
     }
 
@@ -72,11 +75,11 @@ public class Referent {
         return website;
     }
     public void setWebsite(String website) {
-        this.website = StringUtils.requireNotBlankNullable(website, "website");
+        this.website = ValidationUtils.validateString(website, "website", false,255);
     }
 
     public String getNickname() { return nickname; }
     public void setNickname(String nickname) {
-        this.nickname = StringUtils.requireNotBlankNullable(nickname, "nickname");
+        this.nickname = ValidationUtils.validateString(nickname, "nickname", false, 255);
     }
 }
