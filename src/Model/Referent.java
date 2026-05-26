d2824de8be172c1a8231b59228ed39e7ca340a31
Keaexe <187970572+Keaexe@users.noreply.github.com>
2026-05-26 21:04:41 +0200

    feat: add referent now works

--- src/Model/Referent.java
+++ src/Model/Referent.java
@@ -1,10 +1,11 @@
 package Model;
 
+import Exceptions.ValidationException;
 import Utils.ValidationUtils;
-
 import java.time.LocalDate;
 
 public class Referent {
+
     private Integer id;
     private String designation, firstName, lastName;
     private LocalDate birthDate;
@@ -12,8 +13,17 @@ public class Referent {
     private Integer idSchoolOfThought;
     private String website, nickname;
 
-    public Referent(Integer id, String designation, String firstName, String lastName,LocalDate birthDate,
-                    Boolean isAlive, Integer idSchoolOfThought, String website, String nickname){
+    public Referent(
+        Integer id,
+        String designation,
+        String firstName,
+        String lastName,
+        LocalDate birthDate,
+        Boolean isAlive,
+        Integer idSchoolOfThought,
+        String website,
+        String nickname
+    ) throws ValidationException {
         this.id = id;
         setDesignation(designation);
         setFirstName(firstName);
@@ -25,9 +35,27 @@ public class Referent {
         setNickname(nickname);
     }
 
-    public Referent( String designation, String firstName, String lastName,LocalDate birthDate,
-                    Boolean isAlive, Integer idSchoolOfThought, String website, String nickname){
-        this(null,designation,firstName,lastName,birthDate,isAlive,idSchoolOfThought, website, nickname);
+    public Referent(
+        String designation,
+        String firstName,
+        String lastName,
+        LocalDate birthDate,
+        Boolean isAlive,
+        Integer idSchoolOfThought,
+        String website,
+        String nickname
+    ) throws ValidationException {
+        this(
+            null,
+            designation,
+            firstName,
+            lastName,
+            birthDate,
+            isAlive,
+            idSchoolOfThought,
+            website,
+            nickname
+        );
     }
 
     public Integer getId() {
@@ -37,30 +65,55 @@ public class Referent {
     public String getDesignation() {
         return designation;
     }
-    public void setDesignation(String designation){
-        this.designation = ValidationUtils.validateString(designation, "designation", true, 100);
+
+    public void setDesignation(String designation) throws ValidationException {
+        this.designation = ValidationUtils.validateString(
+            designation,
+            "designation",
+            true,
+            100
+        );
     }
 
     public String getFirstName() {
         return firstName;
     }
-    public void setFirstName(String firstName){
-        this.firstName= ValidationUtils.validateString(firstName, "firstName", true, 50);
-    }
 
+    public void setFirstName(String firstName) throws ValidationException {
+        this.firstName = ValidationUtils.validateString(
+            firstName,
+            "firstName",
+            true,
+            50
+        );
+    }
 
     public String getLastName() {
         return lastName;
     }
-    public void setLastName(String lastName){
-        this.lastName = ValidationUtils.validateString(lastName, "lastName", true, 100);
+
+    public void setLastName(String lastName) throws ValidationException {
+        this.lastName = ValidationUtils.validateString(
+            lastName,
+            "lastName",
+            true,
+            100
+        );
     }
 
     public LocalDate getBirthDate() {
         return birthDate;
     }
-    public void setBirthDate(LocalDate birthDate){
-        this.birthDate = ValidationUtils.validateDate(birthDate,"Birth date", true,true,true,false);
+
+    public void setBirthDate(LocalDate birthDate) throws ValidationException {
+        this.birthDate = ValidationUtils.validateDate(
+            birthDate,
+            "Birth date",
+            true,
+            true,
+            true,
+            false
+        );
     }
 
     public boolean getIsAlive() {
@@ -74,12 +127,26 @@ public class Referent {
     public String getWebsite() {
         return website;
     }
-    public void setWebsite(String website) {
-        this.website = ValidationUtils.validateString(website, "website", false,255);
+
+    public void setWebsite(String website) throws ValidationException {
+        this.website = ValidationUtils.validateString(
+            website,
+            "website",
+            false,
+            255
+        );
     }
 
-    public String getNickname() { return nickname; }
-    public void setNickname(String nickname) {
-        this.nickname = ValidationUtils.validateString(nickname, "nickname", false, 255);
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) throws ValidationException {
+        this.nickname = ValidationUtils.validateString(
+            nickname,
+            "nickname",
+            false,
+            255
+        );
     }
 }
--- src/UserInterface/CreateRefPanel.java
+++ src/UserInterface/CreateRefPanel.java
@@ -1,18 +1,25 @@
 package UserInterface;
 
 import Exceptions.DataAccessException;
+import Exceptions.ValidationException;
 import Interfaces.*;
+import Model.Referent;
 import Model.SchoolOfThought;
 import java.awt.BorderLayout;
 import java.awt.Checkbox;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
@@ -21,11 +28,14 @@ import javax.swing.SwingConstants;
 
 public class CreateRefPanel extends JPanel {
 
-    private JLabel designationLabel, firstNameLabel, lastNameLabel, birthDateLabel, isAliveLabel, idSchoolOfThoughtLabel, websiteLabel, nicknameLabel, title;
+    private JLabel designationLabel, firstNameLabel, lastNameLabel, birthDateLabel, isAliveLabel, schoolsOfThoughtLabel, websiteLabel, nicknameLabel, title;
     private JTextField designation, firstName, lastName, website, nickname;
-    private JComboBox<String> idSchoolOfThought;
+    private JComboBox<String> schoolsOfThought;
     private Checkbox isAlive;
     private JSpinner birthDate;
+    private JButton button;
+    private ButtonListener buttonListener;
+    private ArrayList<SchoolOfThought> schools;
 
     public CreateRefPanel(IController controller) throws DataAccessException {
         this.setLayout(new BorderLayout());
@@ -35,7 +45,7 @@ public class CreateRefPanel extends JPanel {
         this.add(title, BorderLayout.NORTH);
 
         var fieldsPanel = new JPanel();
-        fieldsPanel.setLayout(new GridLayout(8, 2, 5, 5));
+        fieldsPanel.setLayout(new GridLayout(9, 2, 5, 5));
 
         firstNameLabel = new JLabel("First name ");
         fieldsPanel.add(firstNameLabel);
@@ -57,8 +67,8 @@ public class CreateRefPanel extends JPanel {
         Date today = new Date();
         SpinnerDateModel dateModel = new SpinnerDateModel(
             today,
-            today,
             null,
+            today,
             Calendar.YEAR
         );
         birthDate = new JSpinner(dateModel);
@@ -75,15 +85,15 @@ public class CreateRefPanel extends JPanel {
         isAlive = new Checkbox();
         fieldsPanel.add(isAlive);
 
-        idSchoolOfThoughtLabel = new JLabel("School of Thought ID ");
-        fieldsPanel.add(idSchoolOfThoughtLabel);
-        ArrayList<SchoolOfThought> schools = controller.getAllSchools();
+        schoolsOfThoughtLabel = new JLabel("School of Thought ID ");
+        fieldsPanel.add(schoolsOfThoughtLabel);
+        schools = controller.getAllSchools();
         var schoolsNames = new String[schools.size()];
         for (int i = 0; i < schools.size(); i++) {
             schoolsNames[i] = schools.get(i).getName();
         }
-        idSchoolOfThought = new JComboBox<String>(schoolsNames);
-        fieldsPanel.add(idSchoolOfThought);
+        schoolsOfThought = new JComboBox<String>(schoolsNames);
+        fieldsPanel.add(schoolsOfThought);
 
         websiteLabel = new JLabel("Website ");
         fieldsPanel.add(websiteLabel);
@@ -95,9 +105,59 @@ public class CreateRefPanel extends JPanel {
         nickname = new JTextField();
         fieldsPanel.add(nickname);
 
+        button = new JButton("Submit");
+        buttonListener = new ButtonListener(controller);
+        button.addActionListener(buttonListener);
+        fieldsPanel.add(button);
+
         var wrapper = new JPanel(new GridBagLayout());
         wrapper.add(fieldsPanel);
         fieldsPanel.setPreferredSize(new java.awt.Dimension(500, 250));
         this.add(wrapper, BorderLayout.CENTER);
+    }
+
+    private class ButtonListener implements ActionListener {
+
+        private IController controller;
+
+        public ButtonListener(IController controller) {
+            this.controller = controller;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            try {
+                controller.addReferent(
+                    new Referent(
+                        designation.getText(),
+                        firstName.getText(),
+                        lastName.getText(),
+                        ((java.util.Date) birthDate.getValue())
+                            .toInstant()
+                            .atZone(java.time.ZoneId.systemDefault())
+                            .toLocalDate(),
+                        isAlive.getState(),
+                        schoolsOfThought.getSelectedIndex(),
+                        website.getText(),
+                        nickname.getText()
+                    )
+                );
+                controller.displayCreateReferent();
+            } catch (ValidationException e) {
+                JOptionPane.showMessageDialog(
+                    null,
+                    "Invalid value entered for referent\n" + e.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+                );
+            } catch (DataAccessException e) {
+                JOptionPane.showMessageDialog(
+                    null,
+                    "Could not create referent",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+                );
+            }
+        }
     }
 }
